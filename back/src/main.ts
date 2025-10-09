2c01f3fa84864796f4c1d3c8f3e27000b9152a80
Sins-Luxuria <pmatteo032002@gmail.com>
2025-10-09 11:42:26 +0200

    feat(back) : add openapi file

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import * as path from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const swaggerDocument = YAML.load(path.join(process.cwd(), 'docs', 'openapi.yaml'));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  
   app.useGlobalPipes(new ValidationPipe({ whitelist: true,forbidNonWhitelisted: true,transform: true,}),);
 
   app.enableCors();

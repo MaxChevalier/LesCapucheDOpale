c73aa6e090684b561a5dd2069692d99cf850f468
Paul <paul.piauger2@gmail.com>
2025-10-27 08:59:00 +0100

    creation de compte fini

--- front/src/app/services/account/account.service.ts
+++ front/src/app/services/account/account.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  private readonly baseUrl = `${environment.apiUrl}/users`;
+  private readonly baseUrl = `/api/users`;
 
   constructor(private readonly http: HttpClient) {  }
 

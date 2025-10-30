25dd5e4a2bf2a157984f291f82d3c659321f8193
Luca Chaboissier <lucachaboissier@outlook.com>
2025-10-30 14:19:33 +0100

    feat(back);gestion-des-consommables;

---
+++ back/src/dto/update-consumable.dto.ts
@@ -0,0 +1,17 @@
+import { IsOptional, IsPositive, IsString } from 'class-validator';
+
+export class UpdateConsumableDto {
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    @IsOptional()
+    @IsPositive()
+    consumableTypeId?: number;
+
+    @IsOptional()
+    quantity?: number;
+
+    @IsOptional()
+    cost?: number;
+}

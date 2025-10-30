9e6a4e280179d2c9ac432e7248f269fde58f50ea
Paul <paul.piauger2@gmail.com>
2025-10-30 09:34:29 +0100

    interceptor

--- front/src/app/interceptor/auth-interceptor.spec.ts
+++ front/src/app/interceptor/auth-interceptor.spec.ts
@@ -1,17 +1,47 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { HttpRequest, HttpHandlerFn, HttpEvent } from '@angular/common/http';
+import { of } from 'rxjs';
 import { authInterceptor } from './auth-interceptor';
 
 describe('authInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+  let next: jasmine.Spy<HttpHandlerFn>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    next = jasmine.createSpy('next').and.callFake((req) => of({} as HttpEvent<any>));
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  afterEach(() => {
+    localStorage.clear();
+  });
+
+  it('should add Authorization header when token exists', (done) => {
+    localStorage.setItem('token', 'abc123');
+
+    const req = new HttpRequest('GET', '/api/data');
+
+    TestBed.runInInjectionContext(() => {
+      authInterceptor(req, next).subscribe(() => {
+        const calledReq = next.calls.mostRecent().args[0] as HttpRequest<any>;
+
+        expect(calledReq.headers.get('Authorization')).toBe('Bearer abc123');
+        done();
+      });
+    });
+  });
+
+  it('should still call next without token', (done) => {
+    const req = new HttpRequest('GET', '/api/data');
+
+    TestBed.runInInjectionContext(() => {
+      authInterceptor(req, next).subscribe(() => {
+        const calledReq = next.calls.mostRecent().args[0] as HttpRequest<any>;
+
+        // Header existe, mais avec Bearer null (car getItem retourne null)
+        expect(calledReq.headers.get('Authorization')).toBe('Bearer null');
+        done();
+      });
+    });
   });
 });
+

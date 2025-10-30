a902284c6b814bbaf55c746d75544e2287248d78
Paul <paul.piauger2@gmail.com>
2025-10-30 09:34:29 +0100

    interceptor

--- front/src/app/interceptor/auth-interceptor.ts
+++ front/src/app/interceptor/auth-interceptor.ts
@@ -1,15 +1,11 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
+export const authInterceptor: HttpInterceptorFn = (req, next)  => {
   const token = localStorage.getItem('token');
-  
-  if (token) {
-    const cloned = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return next(cloned);
-  }
-  return next(req);
+  const authReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+  return next(authReq);
 };
